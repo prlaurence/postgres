db67c6d6e77a9e70b991a352d1fdcc927c397b3e
David Steele <david@pgmasters.net>
2015-02-14 21:26:59 -0500

    Tiny fix.

--- contrib/pg_audit/pg_audit--1.0.0.sql
+++ contrib/pg_audit/pg_audit--1.0.0.sql
@@ -1,4 +1,4 @@
 /* pg_audit/pg_audit--1.0.0.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION pgaudit" to load this file.\quit
+\echo Use "CREATE EXTENSION pg_audit" to load this file.\quit
