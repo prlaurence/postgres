c8007d5a282fcbb3f07e2b94d01cf354d3eb49ec
David Steele <david@pgmasters.net>
2015-04-03 13:09:04 -0400

    Added more options, still not working.

--- ssl.sh
+++ ssl.sh
@@ -4,11 +4,16 @@ USER="dsteele"
 DB_PATH="/var/lib/postgresql/9.4/main"
 USER_PATH="/home/$USER/.postgresql"
 
+SERVER_HOST="server.crunchydata.com"
+
 SUBJ="/C=US/ST=VA/L=Arlington/O=Crunchy Data Solutions/CN="
-SERVER_CNAME="server.crunchydata.com"
+SERVER_CNAME=$SERVER_HOST
 ROOT_CNAME="root-ca"
 IM_CNAME="im-ca"
 CLIENT_CNAME=$USER
+
+SIMPLE="simple"
+INTERMEDIATE="intermediate"
 
 CA="ca"
 ROOT="root"
@@ -26,7 +31,7 @@ function cert
     openssl req -new -sha256 -days 1825 -key $1.key -out $1.csr \
       -subj "$SUBJ$2"
 
-    openssl x509 -req -days 1825 -CA $3.crt -CAkey $3.key -set_serial 01 \
+    openssl x509 -req -days 1825 -CA $3.crt -CAkey $3.key -set_serial $4 \
             -in $1.csr -out $1.crt
     
     rm $1.csr
@@ -35,26 +40,32 @@ function cert
 # Remove current certs and keys
 rm *
 
-# generate passphrase file
-echo "test" > passphrase.txt
-
 # Generate self-signed root CA cert
-openssl req -nodes -new -x509 -keyout $CA.key -out $CA.crt \
+openssl req -nodes -new -sha256 -x509 -keyout $CA.key -out $CA.crt \
    -subj "$SUBJ$ROOT_CNAME"
 
 # Generate intermediate server cert
-cert $SERVER_IM "$SERVER-$IM_CNAME" $CA
+if [ $1 == $INTERMEDIATE ] || [ $1 == $SERVER_IM ]
+    then cert $SERVER_IM "$SERVER-$IM_CNAME" $CA "01";
+fi
 
 # Generate server cert
-#cert $SERVER $SERVER_CNAME $SERVER_IM
-cert $SERVER $SERVER_CNAME $CA
+if [ $1 == $INTERMEDIATE ] || [ $1 == $SERVER_IM ]
+    then cert $SERVER $SERVER_CNAME $SERVER_IM "02";
+    else cert $SERVER $SERVER_CNAME $CA "03";
+fi
+#cert $SERVER $SERVER_CNAME $CA
 
 # Generate intermediate client cert
-cert $CLIENT_IM "$CLIENT-$IM_CNAME" $CA
+if [ $1 == $INTERMEDIATE ] || [ $1 == $CLIENT_IM ]
+    then cert $CLIENT_IM "$CLIENT-$IM_CNAME" $CA "04";
+fi
 
 # Generate client cert
-#cert $CLIENT $CLIENT_CNAME $CLIENT_IM
-cert $CLIENT $CLIENT_CNAME $CA
+if [ $1 == $INTERMEDIATE ] || [ $1 == $CLIENT_IM ]
+    then cert $CLIENT $CLIENT_CNAME $CLIENT_IM "05";
+    else cert $CLIENT $CLIENT_CNAME $CA "06";
+fi
 
 # Stop Postgres
 pg_ctlcluster 9.4 main stop -m fast
@@ -63,10 +74,19 @@ pg_ctlcluster 9.4 main stop -m fast
 rm "$DB_PATH/$CA.crt" "$DB_PATH/$SERVER.key" "$DB_PATH/$SERVER.crt"
 
 # Move files to Postgres
+# if [[ "$1" == "$INTERMEDIATE" ]]
+#     then cat $CLIENT_IM.crt $CA.crt > "$DB_PATH/$CA.crt";
+#     else cp $CA.crt "$DB_PATH/$CA.crt";
+# fi
 cp $CA.crt "$DB_PATH/$CA.crt"
+
 cp $SERVER.key "$DB_PATH/$SERVER.key"
-#cat $SERVER.crt $SERVER_IM.crt > "$DB_PATH/$SERVER.crt"
-cat $SERVER.crt > "$DB_PATH/$SERVER.crt"
+
+if [ $1 == $INTERMEDIATE ] || [ $1 == $SERVER_IM ]
+    then cat $SERVER.crt $SERVER_IM.crt > "$DB_PATH/$SERVER.crt";
+    else cat $SERVER.crt > "$DB_PATH/$SERVER.crt";
+fi
+
 chown postgres:postgres "$DB_PATH/$CA.crt" "$DB_PATH/$SERVER.key" "$DB_PATH/$SERVER.crt"
 chmod 400 "$DB_PATH/$CA.crt" "$DB_PATH/$SERVER.key" "$DB_PATH/$SERVER.crt"
 
@@ -77,10 +97,17 @@ pg_ctlcluster 9.4 main start
 rm "$USER_PATH/$ROOT.crt" "$USER_PATH/$POSTGRESQL.key" "$USER_PATH/$POSTGRESQL.crt"
 
 # Move files to user
-cp $CA.crt "$USER_PATH/$ROOT.crt"
+cp $CA.crt "$USER_PATH/$ROOT.crt";
+
 cp $CLIENT.key "$USER_PATH/$POSTGRESQL.key"
-#cat $CLIENT.crt $CLIENT_IM.crt > "$USER_PATH/$POSTGRESQL.crt"
-cat $CLIENT.crt > "$USER_PATH/$POSTGRESQL.crt"
+
+if [ $1 == $INTERMEDIATE ] || [ $1 == $CLIENT_IM ]
+    then cat $CLIENT.crt $CLIENT_IM.crt > "$USER_PATH/$POSTGRESQL.crt";
+    else cp $CLIENT.crt "$USER_PATH/$POSTGRESQL.crt";
+fi
 
 chown $USER:root "$USER_PATH/$ROOT.crt" "$USER_PATH/$POSTGRESQL.key" "$USER_PATH/$POSTGRESQL.crt"
 chmod 400 "$USER_PATH/$ROOT.crt" "$USER_PATH/$POSTGRESQL.key" "$USER_PATH/$POSTGRESQL.crt"
+
+# Now try to connect
+sudo su - $USER -c 'echo "select count(*) from pg_database" | psql -h localhost postgres'
